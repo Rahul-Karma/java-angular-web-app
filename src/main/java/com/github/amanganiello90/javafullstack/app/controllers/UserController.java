c45d8e5ea8b464253254466fc8631589a886d614
Rahul Mandhan <rahulmandhan071@gmail.com>
2019-07-04 11:24:22 +0530

    Added Cross Origin Support

--- src/main/java/com/github/amanganiello90/javafullstack/app/controllers/UserController.java
+++ src/main/java/com/github/amanganiello90/javafullstack/app/controllers/UserController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="/api")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
 
 	@Autowired
